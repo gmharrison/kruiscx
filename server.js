026f8b185cc6597ac9cef4e46e1f4c4c3f2ed5d9
Gabrielle Harrison <gabrielle@codesters.com>
2018-01-30 16:37:32 -0500

    added cors info

--- server.js
+++ server.js
@@ -45,6 +45,11 @@ if (isDeveloping) {
   }));
 } else {
   app.use(express.static(__dirname + '/dist'));
+  app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
 }
 
 app.post('/form-submit', function(req, res){
